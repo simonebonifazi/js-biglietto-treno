9efc09609601591e0b9017494c322cfb36338198
simonebonifazi <simonbonifraz@gmail.com>
2022-06-21 19:44:04 +0200

    added-logics-steps-to-follow

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* traccia
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va stampato in pagina forma umana (con massimo due decimali, per indicare centesimi sul prezzo).*/
+
+//percorso logico
+/*
+1 - creo una const per contenere la risposta dell'utente ai kilometri da percorrere, la cambio in numero con parseInt e la verifico con console.log(variabile)
+2 - creo il prompt per ottenere la risposta dall'utente
+3 - creo una const per contenere la risposta dell' utente alla domanda sulla sua età, la cambio in numero con parseInt e verifico che tutto funzioni con console.log(..)
+4 - validazione dati inseriti!!
+5 - creo una const per contenere il calcolo del prezzo (0.21 * km)
+6 - creo una let per definire quali valori e quali sconti andrò ad applicare
+7 - calcolo prezzo finale del biglietto   
+*/
+
